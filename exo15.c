0672c8992fff23bcd607b1dc7f27871c2ae39465
RAKOTONDRATREMA Sarobidinirina Fanantenana <sarobidinirinarakoto@gmail.com>
2025-02-10 09:12:28 +0300

    Update exo15.txt

--- exo15.c
+++ exo15.c
@@ -1,16 +1,14 @@
-// Calcul et affichage de la somme S=1+10+10^2+10^3+...+10^n où n est saisi au clavier
-#include <stdio.h>
-#include <math.h>
-int main ()
-{
-    int n , i , somme;
-    printf("Veuillez entrer un nombre entier n :");
-    scanf("%d",&n);
-    somme = 0;
-    for (i=0;i<=n;i++)
-    {
-        somme = somme + pow(10,i);
-    }
-    printf("La somme de 1 jusq'à 10^%d est:%d \n",n,somme);
-    return (0);
-}
+/* Calcul et affichage de la somme s = 1+10+10^2+10^3+... +10^n
+où n est saisi au clavier */
+Algorithme Calcul de la somme s = 1+10+10^2+10^3+... +10^n
+Variables 
+   n, i, somme : entiers;
+Début
+    Écrire ("Veuillez entrer un nombre entier n :");
+    Lire (n);
+    somme <- 0;
+    Pour (i<-1,i<=n,i<-i+1)
+        somme <- somme + pow(10, i);
+    FinPour
+    Écrire ("La somme de 1 jusqu'à 10^",i,"est : ", somme);
+Fin
